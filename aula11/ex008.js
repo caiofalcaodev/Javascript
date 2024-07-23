aabe75a22069afd8e993e21e3ede88405a7131e7
Bowie <caiodlaw@gmail.com>
2024-07-23 20:51:55 -0300

    Exercicios e criação do projeto-hora

---
+++ aula11/ex008.js
@@ -0,0 +1,8 @@
+var vel = 59
+console.log(`a velocidade do seu carro é ${vel}`)
+if (vel > 60) {
+    console.log('Você ultrapassou a velocidade permitida, Multado!')
+} else {
+    console.log('Parabéns pela consciência no trânsito!')
+}
+console.log('Dirija sempre usando cinto de segurança!')
---
+++ aula12/ex15.js
@@ -0,0 +1,28 @@
+var agora = new Date()
+var mesY = agora.getMonth()
+
+console.log(mesY)
+
+switch(mesY) {
+    case 0:
+        console.log('Janeiro')
+        break
+    case 1:
+        console.log('Fevereiro')
+        break
+    case 2:
+        console.log('Março')
+        break
+    case 3:
+        console.log('Abril')
+        break
+    case 4:
+        console.log('Maio')
+        break
+    case 5:
+        console.log('Junho')
+        break
+    case 6:
+        console.log('Julho')
+        break
+    }
